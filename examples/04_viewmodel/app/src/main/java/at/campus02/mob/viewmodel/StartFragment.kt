0234df5571b8f4bcc9b06c9e0bbef8c7bf3239c6
Martin Mair <mair.martin@gmail.com>
2022-05-12 09:45:32 +0200

    Example: ViewModel
    - Task 1

---
+++ examples/04_viewmodel/app/src/main/java/at/campus02/mob/viewmodel/StartFragment.kt
@@ -0,0 +1,29 @@
+package at.campus02.mob.viewmodel
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import at.campus02.mob.viewmodel.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+
+    private lateinit var binding: FragmentStartBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentStartBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.startButton.setOnClickListener {
+            findNavController().navigate(R.id.action_startFragment_to_gameFragment)
+        }
+    }
+}
